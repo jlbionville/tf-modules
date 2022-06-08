bb56af01a2ec6a1b0367fd10ead932ef851929c5
Jean-Louis Bionville <jlbionville@alfaco.fr>
2022-06-08 16:03:06 +0200

    specific file for providers & usage of a profile instead of AWS variables

--- ec2/providers.tf
+++ ec2/providers.tf
@@ -1,6 +1,15 @@
 provider "aws" {
-  
-  access_key="${var.AWS_ACCESS_KEY}"
-  secret_key="${var.AWS_SECRET_KEY}"
-  region="${var.AWS_REGION}"
+  alias   = "aws_ec2"
+  profile = "admincs"
+
+
+  default_tags {
+    tags = {
+
+      Name        = "made-with-terraform"
+      environment = "all"
+      project     = "labs"
+
+    }
+  }
 }
