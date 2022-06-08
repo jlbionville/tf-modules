ddc5b5f8848c156181242e381bb6722c288d31a8
Jean-Louis Bionville <jlbionville@alfaco.fr>
2022-06-08 16:03:47 +0200

    definition of variables for ec2 instances

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,13 +1,19 @@
 variable "ec2_tag_name" {
-  default="noname"
+  default = "noname"
 }
-# variable "AWS_REGION" {
-  
-# }
 
-# variable "AWS_ACCESS_KEY" {
-  
-# }
-# variable "AWS_SECRET_KEY" {
-  
-# }
+variable "public_instance"{
+  default = true
+}
+variable "instance_type" {
+  default="t2.micro"
+}
+variable "key_pair" {
+  default =""
+}
+variable ami_name {
+  default="ami-0d3c032f5934e1b41"
+}
+variable ec2_role{
+  default = ""
+}
