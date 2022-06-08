632b70ce55bc346a868f1dca7fd684739be35f71
Jean-Louis Bionville <jlbionville@alfaco.fr>
2022-06-08 17:44:17 +0200

    init du module S3

---
+++ s3/main.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "default_bucket" {
+  bucket = local.bucket_name
+}
+
+locals {
+  # faire un test pour prendre la surcharge => var.bucket_name
+  bucket_name = format("%s-%s", var.company, var.project_name)
+}
---
+++ s3/variables.tf
@@ -0,0 +1,4 @@
+# A utiliser si on ne veut pas la regle fonctionnelle
+variable "bucket_name" {
+  default="default-bucket"
+}
