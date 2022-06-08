b4526623cf87eefe1d69dd3e770c1d34b3dbbd86
Jean-Louis Bionville <jlbionville@alfaco.fr>
2022-06-08 12:33:11 +0200

    definition du nom de l'instance ec2

--- ec2/workspace/ec2.tf
+++ ec2/workspace/ec2.tf
@@ -2,9 +2,11 @@ resource "aws_instance" "wordpress" {
   ami           = "ami-0d3c032f5934e1b41" # Amazon Linux in us-east-1, update as per your region
   instance_type = "t2.micro"
   #iam_instance_profile ="${aws_iam_instance_profile.ec2_profile.name}"
-  key_name= "cloudshopping"
+  
   tags = {
     labs   = "ec2"
     number = "01"
     project ="chef"
+    Name=var.ec2_tag_name
   }
+}
